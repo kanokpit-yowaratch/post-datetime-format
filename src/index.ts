930e1b681664ec149a66cbfb13b438a7285e0676
Kanokpit <kanokpit.y@gmail.com>
2024-11-06 05:37:12 +0700

    fix: handle if param value is invalid

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,29 @@ const postDateTimeFormat = (
 	timeZone: string = 'Asia/Bangkok',
 	local: string = 'TH-th'
 ) => {
-	const options: Intl.DateTimeFormatOptions = {
-		month: "short",
-		day: "numeric",
-		year: "numeric",
-		...(time && {
-			minute: "2-digit",
-			hour: "2-digit",
-		}),
-		timeZone
-	};
-	const day = new Date(date)
-		.toLocaleDateString(local, options)
-		.toLocaleLowerCase();
-	return day;
+	try {
+		const options: Intl.DateTimeFormatOptions = {
+			month: "short",
+			day: "numeric",
+			year: "numeric",
+			...(time && {
+				minute: "2-digit",
+				hour: "2-digit",
+			}),
+			timeZone
+		};
+		const day = new Date(date)
+			.toLocaleDateString(local, options)
+			.toLocaleLowerCase();
+		return day;
+	} catch (error: any) {
+		if (error.message) {
+			console.log(error.message);
+		} else {
+			console.log(error);
+		}
+		return date;
+	}
 }
 
 export default postDateTimeFormat;
